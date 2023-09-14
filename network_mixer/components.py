0eda2db918a2ab0d8f9e9812dc28fd72ef0d9044
maksymilian <maksymilian3563@gmail.com>
2023-09-14 10:35:51 +0200

    Second half of .NET.XML file reconstruction

--- network_mixer/components.py
+++ network_mixer/components.py
@@ -6,7 +6,8 @@
 
 from enum import Enum
 
-import TIIIP.simulation.vehicle_classes
+import TIIIP
+from TIIIP.simulation import Vehicle
 
 
 class Environment:
@@ -15,7 +16,7 @@ class Environment:
     """
     xml_data = {}
     net_data = {}
-    location = {}
+    location_data = {}
 
     def load_xml_data(self, xml_string:str):
         xml_string = xml_string.split('\"')
@@ -24,11 +25,36 @@ class Environment:
             'encoding': xml_string[3]
         }
 
-    def load_net_data(self, net_dict:dict):
-        self.net_data = net_dict
+    def load_net_data(self, net_string:str):
+        net_string = net_string.split('\"')
+        self.net_data = {
+            'version': net_string[1],
+            'junctionCornerDetail': net_string[3],
+            'limitTurnSpeed': net_string[5],
+            'xmlns:xsi': net_string[7],
+            'xsi:noNamespaceSchemaLocation': net_string[9],
+        }
 
     def load_location_data(self, location_dict:dict):
-        self.location = location_dict
+        self.location_data = location_dict
+
+    def get_xml_xml_line_attribs(self):
+        result = ''
+        for key, value in self.xml_data.items():
+            result += '{}=\"{}\" '.format(key, value)
+        return result
+
+    def get_net_xml_line_attribs(self):
+        result = ''
+        for key, value in self.net_data.items():
+            result += '{}=\"{}\" '.format(key, value)
+        return result
+
+    def get_location_xml_line_attribs(self):
+        result = ''
+        for key, value in self.location_data.items():
+            result += '{}=\"{}\" '.format(key, value)
+        return result
 
 
 class SpreadType(Enum):
@@ -98,6 +124,12 @@ class Component:
             return self.get_schema()
         return {}
 
+    @property
+    def level(self):
+        if hasattr(self, 'get_level'):
+            return self.get_level()
+        return 0
+
     def clean(self):
         for key, value in self.__dict__.items():
             try:
@@ -105,23 +137,68 @@ class Component:
             except KeyError:
                 pass
 
+    # A function for building .NET.XML files from Components
+    # Currently tested on just one case
     def get_xml_line(self):
-        file_string = '<{} '.format(self.__class__.__name__.lower())
-        for key, value in self.__dict__.items():
+        file_string = '{}<{} '.format('    ' * self.level, self.__class__.__name__.lower())
+        end_string = '/>\n'
+        for key, value in self.__dict__.items():        
+            if key[0] == '_':
+                if key == '_lanes' and value:
+                    end_string = '>\n'
+                    for lane in self._lanes:
+                        end_string += lane.get_xml_line()
+                    end_string += '{}</edge>\n'.format('    ' * self.level)
+                if key == '_requests' and value:
+                    end_string = '>\n'
+                    for request in self._requests:
+                        end_string += request.get_xml_line()
+                    end_string += '{}</junction>\n'.format('    ' * self.level)
+                continue
             formatted_value = value
 
-            if type(value) is bool:
+            if isinstance(value, Enum):
+                formatted_value = value.value
+
+            elif isinstance(value, Type):
+                formatted_value = value.id
+
+            elif isinstance(value, bool):
                 formatted_value = {True: '1', False: '0'}[value]
 
-            if type(value) is list:
+            elif isinstance(value, list):
                 value_list = value
+                if not value:
+                    continue
                 if value_list:
-                    if type(value_list[0]) is type:
+                    if isinstance(value_list[0], str):
+                        pass
+                    elif isinstance(value_list[0], tuple):
+                        if isinstance(value_list[0][0], float):
+                            value_list = ['{},{}'.format(pair[0], pair[1]) for pair in value]
+                    elif type(value_list[0]) is not type:
+                        continue
+                    elif issubclass(value_list[0], Vehicle):
                         value_list = [v.__name__.lower() for v in value]
                 formatted_value = ' '.join(value_list)
 
             file_string += '{}=\"{}\" '.format(key, formatted_value)
-        return file_string + '>\n'
+        return file_string + end_string
+
+    # Functions for specific yet common schema purposes
+    def get_fixed_bool(self, bool_string:str):
+        return False if bool_string == '0' else True
+
+    def get_id_list(self, initial_string:str):
+        return initial_string.split(' ')
+
+    def get_shape_list(self, initial_string:str):
+        result = []
+        split_pairs = initial_string.split()
+        for pair in split_pairs:
+            latlon = tuple([float(coord) for coord in pair.split(',')])
+            result.append(latlon)
+        return result
 
     def get_vehicle_classes(self, allow_string:str):
         result = []
@@ -137,13 +214,16 @@ class Component:
 
 class Type(Component):
 
+    def get_level(self):
+        return 1
+
     def get_schema(self):
         schema = {
             'id': str,
             'priority': int,
             'numLanes': int,
             'speed': float,
-            'oneway': bool,
+            'oneway': self.get_fixed_bool,
             'width': float,
             'allow': self.get_vehicle_classes,
             'disallow': self.get_vehicle_classes,
@@ -158,8 +238,13 @@ class Edge(Component):
      applying mutations and generally manipulating possible edges.
     """
 
-    lanes = []
-    outside_connection = False
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._lanes = []
+        self._outside_connection = False
+
+    def get_level(self):
+        return 1
 
     def get_schema(self):
         schema = {
@@ -172,7 +257,7 @@ class Edge(Component):
             'speed': float,
             'priority': int,
             'length': float,
-            'shape': list[float],  # TODO
+            'shape': self.get_shape_list,
             'spreadType': SpreadType,
             'allow': self.get_vehicle_classes,
             'disallow': self.get_vehicle_classes,
@@ -192,6 +277,9 @@ class Lane(Component):
      This class is used for quick analysis of lane.
     """
 
+    def get_level(self):
+        return 2
+
     def get_schema(self):
         schema = {
             'id': str,
@@ -204,9 +292,9 @@ class Lane(Component):
             'width': float,
             'length': float,
             'endOffset': float,
-            'shape': list,  # TODO
+            'shape': self.get_shape_list,
             'type': self.get_type_class,
-            'acceleration': bool,
+            'acceleration': self.get_fixed_bool,
         }
         return schema
 
@@ -216,7 +304,12 @@ class Junction(Component):
      Junctions taken from the official SUMO docs: https://sumo.dlr.de/docs/Networks/SUMO_Road_Networks.html#junctions_and_right-of-way
     """
 
-    requests = []
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._requests = []
+
+    def get_level(self):
+        return 1
 
     def get_schema(self):
         schema = {
@@ -225,21 +318,24 @@ class Junction(Component):
             'x': float,
             'y': float,
             'z': float,
-            'incLanes': list,  # TODO
-            'intLanes': list,  # TODO
-            'shape': list,
+            'incLanes': self.get_id_list,
+            'intLanes': self.get_id_list,
+            'shape': self.get_shape_list,
         }
         return schema
 
 
 class Request(Component):
 
+    def get_level(self):
+        return 2
+
     def get_schema(self):
         schema = {
             'index': int,
             'response': str,
             'foes': str,
-            'cont': bool,
+            'cont': self.get_fixed_bool,
         }
         return schema
 
@@ -248,6 +344,9 @@ class Connection(Component):
     """
      Connection taken from the official SUMO docs: https://sumo.dlr.de/docs/Networks/SUMO_Road_Networks.html#connections
     """
+
+    def get_level(self):
+        return 1
 
     def get_schema(self):
         schema = {
@@ -266,9 +365,12 @@ class Connection(Component):
 
 class Roundabout(Component):
 
+    def get_level(self):
+        return 1
+
     def get_schema(self):
         schema = {
-            'nodes': list,  # TODO
-            'edges': list,  # TODO
+            'nodes': self.get_id_list,
+            'edges': self.get_id_list,
         }
         return schema
--- network_mixer/net_xml_parser.py
+++ network_mixer/net_xml_parser.py
@@ -27,11 +27,14 @@ def load_base_file():
 
     f = open(BASE_ROAD_FILE_PATH, 'r')
     xml_data = f.readline()
+    for line in f:
+        if line[:4] == '<net':
+            net_data = line
     f.close()
 
     environment = Environment()
     environment.load_xml_data(xml_data)
-    environment.load_net_data(root.attrib)
+    environment.load_net_data(net_data)
 
     components = {tag: {} for tag in ['type', 'edge', 'junction', 'connection', 'roundabout']}
 
@@ -51,16 +54,23 @@ def load_base_file():
             components[child.tag][child.attrib[identifier]] = getattr(TIIIP.network_mixer, child.tag.title())(**child.attrib)
         else:
             components[child.tag][len(components[child.tag])] = getattr(TIIIP.network_mixer, child.tag.title())(**child.attrib)
-    rebuild_file(environment, components, 'test.xml')
-    print(environment.xml_data)
-    # return environment, components
+
+        for grandchild in child:
+            if grandchild.tag == 'lane':
+                components[child.tag][child.attrib['id']]._lanes.append(Lane(**grandchild.attrib))
+            if grandchild.tag == 'request':
+                components[child.tag][child.attrib['id']]._requests.append(Request(**grandchild.attrib))
+
+    return environment, components
 
 
 def rebuild_file(environment:Environment, components:dict, save_file_path:str):
     with open(save_file_path, 'w+') as f:
-        f.write('<?xml version="{}" encoding="{}"?>\n\n'.format(environment.xml_data['version'], environment.xml_data['encoding']))
-        f.write('<net ')
+        f.write('<?xml {}?>\n\n'.format(environment.get_xml_xml_line_attribs()))
+        f.write('<net {}>\n\n'.format(environment.get_net_xml_line_attribs()))
+        f.write('    <location {}/>\n\n'.format(environment.get_location_xml_line_attribs()))
         for key, value in components.items():
-            print('{}\n{}\n\n'.format(key, value))
             for _, component in value.items():
                 f.write(component.get_xml_line())
+            f.write('\n')
+        f.write('\n</net>')
