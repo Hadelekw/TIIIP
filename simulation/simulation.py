522881fd8a66135e14e08c4ab6a05321bf25ba10
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2023-11-07 11:23:23 +0100

    Added CO2 emissions data collection

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -14,7 +14,9 @@ from settings import BASE_SUMO_CONFIG_FILE_PATH
 
 
 DATA_SCHEMA = {
-    'vehicles_per_step': []
+    'vehicles_per_step': [],
+    'co2_emissions_per_step': [],
+    # 'average_co2_emissions_per_step_per_liter': [],
 }
 
 
@@ -23,8 +25,17 @@ def run():
 
     data = DATA_SCHEMA
 
+    bot_left, top_right = traci.simulation.getNetBoundary()
+    bot_left = list(bot_left); top_right = list(top_right)
+    for i in range(len(top_right)):
+        bot_left[i] /= 10
+        top_right[i] /= 10
+    total_area_of_simulation = (top_right[0] - bot_left[0]) * (top_right[1] - bot_left[1])
+
     for step in range(1000):
-        data['vehicles_per_step'].append(len(traci.vehicle.getIDList()))
+        data['vehicles_per_step'].append(traci.simulation.getMinExpectedNumber())
+        data['co2_emissions_per_step'].append(sum([traci.vehicle.getCO2Emission(vehicle_id) for vehicle_id in traci.vehicle.getIDList()]))
+        # data['average_co2_emissions_per_step_per_liter'].append(list([co2_emission / area_of_simulation for co2_emission in data['co2_emissions_per_step']]))
         traci.simulationStep()
     
     traci.close()
