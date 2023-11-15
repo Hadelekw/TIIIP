7a09a6a7a5ebdf3fabcc518332432cf857d84adf
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2023-11-15 16:38:50 +0100

    WIP population generation

--- genetics/__init__.py
+++ genetics/__init__.py
@@ -1,2 +1,4 @@
 from .evaluation import *
 from .specimen import *
+from .generate_population import *
+
--- genetics/generate_population.py
+++ genetics/generate_population.py
@@ -2,7 +2,22 @@
  File containing function for generating a new population.
 """
 
+import sys
+sys.path.append('../')
+
+import network_mixer.mixer as mixer
+from simulation import DATA_SCHEMA
+from specimen import *
+from evaluation import evaluate
+
 
 def generate_population(population, data):
     result = []
+
+    scores = [evaluate(sim_result) for sim_result in data.values()]
+    population_scores = {specimen: score for specimen, score in zip(population, scores)}
+
+    # figure out how to sort them and remove the weaklings
+    # then create new population by crossing and mutations
+
     return result
